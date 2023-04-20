7993020a54ac28be6f24043c8aa95627bb87e658
Nima Boscarino <nima@huggingface.co>
2023-04-20 09:39:57 -0700

    async scraper updates

--- async_api_scrapes/run.py
+++ async_api_scrapes/run.py
@@ -1,59 +1,76 @@
+from datasets import load_from_disk, load_dataset
+
+import json
+
 import asyncio
 import aiohttp
 from aiolimiter import AsyncLimiter
-from datasets import load_from_disk
 
-import math
-import time
-from typing import List
-
-limiter = AsyncLimiter(1, 0.125)
 TOKEN = "<SPAWNING API TOKEN>"
 headers = {"Authorization": f"API {TOKEN}"}
 
-BATCH_SIZE = 1000
-IMAGE_FEATURE = "URL"  # For LAION2B-en
+OPT_IN_OUT_URLS_SCAN_MAX_CONCURRENT_REQUESTS_NUMBER = 100
+OPT_IN_OUT_URLS_SCAN_MAX_REQUESTS_PER_SECOND = 50
 
-ds = load_from_disk("./laion2b-en-small")
-num_batches = math.floor(len(ds) / BATCH_SIZE)
-shards = [ds.shard(num_batches, index=i) for i in range(0, num_batches)]
+BATCH_SIZE = 100000
+CHUNK_SIZE = 1000
+IMAGE_FEATURE = "URL"
+
+OPT_IN_COUNT = 0
+OPT_OUT_COUNT = 0
 
 
-async def check_spawning(image_urls: List[str], semaphore) -> bytes:
+async def check_spawning(image_urls, semaphore, limiter):
     url = f"https://opts-api.spawningaiapi.com/api/v2/query/urls"
     async with aiohttp.ClientSession(headers=headers) as session:
         await semaphore.acquire()
         async with limiter:
-            # print("\n".join(image_urls))
-            # print(f"Begin downloading {url} {(time.perf_counter() - s):0.4f} seconds")
             async with session.post(
                 url=url,
-                data="\n".join(image_urls)
+                data="\n".join(image_urls).encode("utf-8")
             ) as resp:
                 content = await resp.read()
                 semaphore.release()
-                return content
+                return json.loads(content)
 
 
-async def opt_in_out_task(image_urls: List[str], semaphore) -> None:
-    content = await check_spawning(image_urls, semaphore)
-    # print(content)
-    # await upsert_to_database?
-    # or maybe await ...add_to_mapped_dataset?
-    # await write_to_file(pep_number, content)...
-
-
-async def main() -> None:
+async def opt_in_out_task(data_items) -> (int, int, int):
     tasks = []
-    semaphore = asyncio.Semaphore(value=100)
+
+    semaphore = asyncio.Semaphore(value=OPT_IN_OUT_URLS_SCAN_MAX_CONCURRENT_REQUESTS_NUMBER)
+    limiter = AsyncLimiter(OPT_IN_OUT_URLS_SCAN_MAX_REQUESTS_PER_SECOND, time_period=1)
+
+    shards = [data_items["URL"][i:i + CHUNK_SIZE] for i in range(0, len(data_items["URL"]), CHUNK_SIZE)]
 
     for shard in shards:
-        batch = shard[IMAGE_FEATURE]
-        tasks.append(opt_in_out_task(batch, semaphore))
+        tasks.append(asyncio.create_task(check_spawning(shard, semaphore, limiter)))
     await asyncio.wait(tasks)
 
+    content = [url for task in tasks for url in task.result()["urls"]]
+
+    opt_in = [x["optIn"] for x in content]
+    opt_out = [x["optOut"] for x in content]
+
+    return {"OPT_IN": opt_in, "OPT_OUT": opt_out}
+
+
+def async_mapping(data_items):
+    global OPT_IN_COUNT, OPT_OUT_COUNT
+
+    results = asyncio.run(opt_in_out_task(data_items))
+
+    OPT_IN_COUNT = OPT_IN_COUNT + sum(results["OPT_IN"])
+    OPT_OUT_COUNT = OPT_OUT_COUNT + sum(results["OPT_OUT"])
+
+    return results
+
+
 if __name__ == "__main__":
-    s = time.perf_counter()
-    asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"Execution time: {elapsed:0.2f} seconds.")
+    ds = load_from_disk("./laion2b-en-small")
+    # ds = load_dataset("laion/laion2B-en", split="train", num_proc=2)
+    ds_opts = ds.map(batched=True, batch_size=BATCH_SIZE, function=async_mapping, remove_columns=ds.column_names)
+
+    with open("./results", "w") as f:
+        f.write(f"OPT_IN_COUNT: {OPT_IN_COUNT}\n")
+        f.write(f"OPT_OUT_COUNT: {OPT_OUT_COUNT}\n")
+        f.write(f"LENGTH: {len(ds_opts)}\n")
